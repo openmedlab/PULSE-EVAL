68fc500600cd9123d81a9a66c9c2488a7e875882
Luo-Kaiyi <1017076453@qq.com>
2023-12-09 23:46:25 +0800

    PULSE-Eval First Version 20231209 -- Luo Kaiyi

---
+++ eval/eval_utils.py
@@ -0,0 +1,160 @@
+from glob import glob
+import json
+from collections import OrderedDict, defaultdict
+from datasets import load_dataset
+import regex as re
+from pandas import DataFrame
+from nltk.translate.bleu_score import corpus_bleu, sentence_bleu
+import numpy as np
+import os
+
+DATASET_LIST = []
+GUIDE_LIST = []
+
+
+def get_all_predict_data(all_predict_paths):
+    all_predict_data = OrderedDict()
+    for path in all_predict_paths:
+        model_name = path.split("/")[-2]
+        all_predict_data[model_name] = []
+        with open(path, "r", encoding="utf8") as f:
+            for line in f:
+                line = line.rstrip()
+                if len(line) == 0:
+                    continue
+                line = json.loads(line)
+                all_predict_data[model_name].append(line)
+
+    # check len
+    d_len = None
+    for item in all_predict_data.values():
+        if d_len is None:
+            d_len = len(item)
+            
+        assert d_len == len(item)
+
+    # check q
+    for idx in range(d_len):
+        q = None
+        for model_name in all_predict_data.keys():
+            if q is None:
+                q = all_predict_data[model_name][idx]['question']
+            
+            assert q == all_predict_data[model_name][idx]['question']
+
+    print("load predict success")
+    return all_predict_data
+ 
+      
+def get_table_rank(table, dataset_list):
+    columns = table.columns
+    datasets = []
+    for col in columns:
+        if col in dataset_list:
+            datasets.append(col)
+    for d in datasets:
+        array = np.array(table[d])
+        ranks = len(array) - array.argsort().argsort()
+        table[d] = ranks.tolist()
+    
+    return table
+  
+    
+def acc_evaluation(
+    selected_models,
+    dataset_lists,
+    predict_dir = 'eval/predicted/',
+):
+    statistic = defaultdict(dict)
+    for dataset_name in dataset_lists:
+        for model_name in selected_models:
+            dataset = load_dataset(
+                'json',
+                data_files=f"{predict_dir + model_name}/{dataset_name}.jsonl", 
+                split='train'
+            )
+            statistic[model_name][f"{dataset_name} (Acc)"] = acc_triage(dataset)
+            p, r, f1 = f1_triage(dataset)
+            statistic[model_name][f"{dataset_name} (P)"] = p
+            statistic[model_name][f"{dataset_name} (R)"] = r
+            statistic[model_name][f"{dataset_name} (F1)"] = f1
+            
+    table = DataFrame(
+        [
+            {
+                'Model Name': k,
+                **{dk: dv for dk, dv in v.items()}
+            }
+            for k, v in statistic.items()
+        ]
+    )
+    return table
+
+
+def acc_triage(dataset):
+    acc_cnt = 0
+    
+    for item in dataset:
+        refs = set(item['reference_answer'].replace(" ", "").split("|"))
+        prds = set(item['predict_answer'].replace(" ", "").split("|"))
+        acc_cnt += int(refs == prds)
+        
+    return acc_cnt / len(dataset)
+
+
+def f1_triage(dataset):
+    tp = 0  # ture predicted number 
+    ap = 0  # all predicted number
+    ag = 0  # all ground-truth number
+    
+    for item in dataset:
+        refs = item['reference_answer'].replace(" ", "").split("|")
+        prds = item['predict_answer'].replace(" ", "").split("|")
+
+        ap += len(prds)
+        ag += len(refs)
+        
+        for r in refs:
+            tp += int(r in prds)
+    p = tp / (ap + 1e-10)
+    r = tp / (ag + 1e-10)
+    f1 = (2 * p * r) / (p + r + 1e-10)
+    
+    return p, r, f1
+
+
+def merge_table(elo_table, acc_table, merge_names=[]):
+    for i in range(len(elo_table)):
+        for j in range(len(acc_table)):
+            if elo_table.loc[i, 'Model Name'] == acc_table.loc[j, 'Model Name']:
+                for name in merge_names:
+                    elo_table.loc[i, name] = acc_table.loc[j, name]
+    return elo_table
+
+
+def add_avg_rank(rank_table, datasets):
+    for i in range(len(rank_table)):
+        avg_rank = 0.0
+        for dataset in datasets:
+            avg_rank += rank_table.loc[i, dataset]
+        rank_table.loc[i, 'AVG Rank'] = avg_rank / len(datasets)
+
+    return rank_table
+
+
+def write_to_md(
+    table, 
+    to_path="eval_table.md",
+    sort_value=None, 
+    ascending=True, 
+    floatfmt='.02f', 
+    ):
+    if sort_value:
+        table = table.sort_values(sort_value, ascending=ascending)
+    md_table = table.to_markdown(index=False, floatfmt=floatfmt)
+    
+    if os.path.exists(to_path):
+        os.remove(to_path)
+    with open(to_path, 'w') as f:
+        f.write(md_table)
+    print(f"Write evaluation results to {to_path}.")
---
+++ eval/main.py
@@ -0,0 +1,81 @@
+
+import os
+import random
+import openai
+from copy import deepcopy
+os.environ['OPENAI_API_BASE'] = "https://api.emabc.xyz/v1"
+api_key = ''
+openai.api_key = api_key
+
+from elo_utils import (
+    construct_elo_inputs, 
+    elo_evaluation, 
+    call_evaluator
+    )
+from eval_utils import (
+    acc_evaluation, 
+    merge_table, 
+    get_table_rank,
+    write_to_md,
+    add_avg_rank
+    )
+
+
+
+if __name__ == "__main__":
+    random.seed(42)
+    # Models for evaluation
+    eval_models = [
+        'ChatGPT',
+        'GPT-4',
+        'PULSE-Pro',
+        'PULSE-OS',
+        'Baichuan2',
+        'ChatGLM3',
+        'HuatuoGPT2',
+        'QiZhenGPT',
+        'BenTsao',
+        'MING',
+        'BianQue2',
+        'DoctorGLM',
+    ]
+    
+    # Datasets for evaluation
+    eval_datasets = [
+        'MedQA-USMLE',
+        'MedQA-Mainland',
+        'PromptCBLUE',
+        'WebMedQA',
+        'CheckupQA',
+        'MedicineQA',
+        'DialogSumm',
+        'MedTriage',
+    ]
+    # Construct Elo inputs. 
+    # If you have already run this, you should comment out this line.
+    # construct_elo_inputs(eval_models, eval_datasets, start_p_idx=0)
+    
+    # Call Evaluator API to get Elo results.
+    # If you have already run this, you should comment out this line.
+    # call_evaluator(
+    #     in_dir="eval/elo/elo_inputs",
+    #     to_dir="eval/elo/elo_outputs",
+    #     num_proc=50,
+    #     )
+    
+    # Get evaluation results
+    eval_datasets.remove("MedTriage")
+    elo_table = elo_evaluation(eval_models, eval_datasets)
+    acc_table = acc_evaluation(eval_models, ["MedTriage"])
+    all_table = merge_table(elo_table.drop("ALL", axis=1), acc_table, ['MedTriage (F1)'])
+    # score to rank
+    datasets = eval_datasets + ['MedTriage (F1)']
+    rank_table = get_table_rank(deepcopy(all_table), datasets)
+    rank_table = add_avg_rank(rank_table, datasets)
+    
+    all_table = merge_table(all_table, rank_table, ['AVG Rank'])
+    
+    write_order = ["Model Name", "AVG Rank"] + datasets 
+    write_to_md(all_table[write_order], to_path="score_table.md", sort_value="AVG Rank")
+    write_to_md(rank_table[write_order], to_path="rank_table.md", sort_value="AVG Rank")
+        
